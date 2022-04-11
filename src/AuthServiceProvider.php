2fec21256f0c88f48c8014598ce22644aaff3205
amir <amirilidan78@gmail.com>
2022-04-11 11:23:47 +0430

    qwe

--- src/AuthServiceProvider.php
+++ src/AuthServiceProvider.php
@@ -1,23 +1,27 @@
 <?php
 
-namespace Amirilidan78\AuthToken\Providers;
+namespace Amirilidan78\AuthToken;
 
 use Amirilidan78\AuthToken\Middleware\AuthTokenMiddleware;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Sanctum\Sanctum;
 
 class AuthServiceProvider extends ServiceProvider
 {
     public function register()
     {
         app('router')->aliasMiddleware('authentication', AuthTokenMiddleware::class);
+
+        if (! app()->configurationIsCached())
+            $this->mergeConfigFrom(__DIR__.'/../config/auth_token.php', 'auth_token');
     }
 
     public function boot()
     {
-        $this->loadMigrationsFrom(__DIR__.'/../Migrations');
+        $this->loadMigrationsFrom(__DIR__.'/../../database/migrations');
 
         $this->publishes([
-            __DIR__.'/../Configs/token_auth.php' => config_path('auth_token.php'),
+            __DIR__.'/../../config/auth_token.php' => config_path('auth_token.php'),
         ]);
     }
 
